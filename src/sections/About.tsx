4ca1b7b9724baa18ea6d97c7fd3d150b7b5b0b6d
AdrianPrisacaru <adrian.prisacaru@thinslices.com>
2025-06-13 13:10:32 +0100

    Finish About page and small changes on the tablet and desktop sizes

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,13 +1,113 @@
+import { Card } from "@/components/Card";
 import { SectionHeader } from "@/components/SectionHeader";
+import bookImage from "@/assets/images/book-cover.png";
+import Image from "next/image";
+import JavascriptIcon from "@/assets/icons/square-js.svg";
+import HTMLIcon from "@/assets/icons/html5.svg";
+import CssIcon from "@/assets/icons/css3.svg";
+import ReactIcon from "@/assets/icons/react.svg";
+import GithubIcon from "@/assets/icons/github.svg";
+import mapImage from "@/assets/images/map.png";
+import smileMemoji from "@/assets/images/memoji-smile.png";
+import { CardHeader } from "@/components/CardHeader";
+import { ToolBoxItems } from "@/components/ToolBoxItems";
+
+const toolBoxItems = [
+  { title: "Javascript", iconType: JavascriptIcon },
+  { title: "NextJs", iconType: CssIcon },
+  { title: "HTML5", iconType: HTMLIcon },
+  { title: "React", iconType: ReactIcon },
+  { title: "Github", iconType: GithubIcon },
+  { title: "Typescript", iconType: CssIcon },
+  { title: "Jest", iconType: CssIcon },
+  { title: "MaterialUI", iconType: CssIcon },
+  { title: "Tailwind", iconType: CssIcon },
+];
+
+const hobbies = [
+  { title: "Horse Riding", emoji: "🏇", left: "5%", top: "5%" },
+  { title: "Painting", emoji: "🎨", left: "40%", top: "15%" },
+  { title: "Hiking", emoji: "🥾", left: "10%", top: "35%" },
+  { title: "Boardgames", emoji: "🧩", left: "35%", top: "40%" },
+  { title: "Photography", emoji: "📸", left: "70%", top: "45%" },
+  { title: "Drawing", emoji: "✏️", left: "5%", top: "65%" },
+  { title: "Cooking", emoji: "🍳", left: "45%", top: "70%" },
+  { title: "Dancing", emoji: "🕺", left: "70%", top: "10%" },
+];
 
 export const AboutSection = () => {
   return (
-    <div>
-      <SectionHeader
-        eyebrow="About me"
-        title="A Glimpse Into My World"
-        description=""
-      />
+    <div className="py-20 lg:py-28">
+      <div className="container">
+        <SectionHeader
+          eyebrow="About me"
+          title="A Glimpse Into My World"
+          description="Learn more about who I am, what I do, and what inspires me."
+        />
+        <div className="mt-20 flex flex-col gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-5 gap-8 lg:grid-cols-3">
+            <Card className="h-[320px] md:col-span-2 lg:col-span-1">
+              <CardHeader
+                title="My Reads"
+                description="Explore the books shaping my perspectives"
+              />
+              <div className="w-40 mx-auto mt-2 md:mt-0">
+                <Image src={bookImage} alt="Book cover" />
+              </div>
+            </Card>
+            <Card className="h-[320px] md:col-span-3 lg:col-span-2">
+              <CardHeader
+                title="My ToolBox"
+                description="Explore the technologies and tools I use to craft exceptional
+                digital experiences."
+              />
+              <ToolBoxItems items={toolBoxItems} />
+              <ToolBoxItems
+                items={toolBoxItems}
+                className="mt-6"
+                itemsWrapperClassName={"-translate-x-1/2"}
+              />
+            </Card>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-8">
+            <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
+              <CardHeader
+                className="px-6 py-6"
+                title="Beyond the Code"
+                description="Explore my interests and hobbies beyond the digital realm."
+              />
+              <div className="relative flex-1">
+                {hobbies.map((hobby) => (
+                  <div
+                    key={hobby.title}
+                    className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+                    style={{ left: hobby.left, top: hobby.top }}
+                  >
+                    <span className="font-medium text-gray-950">
+                      {hobby.title}
+                    </span>
+                    <span>{hobby.emoji}</span>
+                  </div>
+                ))}
+              </div>
+            </Card>
+            <Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
+              <Image
+                src={mapImage}
+                alt="map"
+                className="h-full w-full object-cover object-left-top"
+              />
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+                <Image
+                  src={smileMemoji}
+                  alt="smiling memoji"
+                  className="size-20"
+                />
+              </div>
+            </Card>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
